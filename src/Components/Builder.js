56cdc8a97fa84828678de731669726d171554a16
arthur <arthur.roth.07@gmail.com>
2023-02-05 18:31:05 -0800

    made home, builder, header

---
+++ src/Components/Builder.js
@@ -0,0 +1,9 @@
+function Builder() {
+    return (
+        <div className="Builder">
+            "builder"
+        </div>
+    )
+}
+
+export default Builder;
---
+++ src/Components/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="Home">
+            "text"
+        </div>
+    )
+}
+
+export default Home;
