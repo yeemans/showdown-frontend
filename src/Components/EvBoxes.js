53a77ee8a772fed98eaf75771f8ba16be421ec49
arthur <arthur.roth.07@gmail.com>
2023-02-14 17:13:33 -0800

    moved ev inputs into slider component

--- src/Components/EvBoxes.js
+++ src/Components/EvBoxes.js
@@ -1,22 +1,22 @@
 import { useState } from "react";
+import EvInput from "./EvInput";
 
 function EvBoxes() { 
     const [evs, setevs] = useState({"HP": 0 , "Atk": 0, "Def": 0, "SpA": 0, "SpD": 0 , "Spe": 0});
 
-    function updateEv(ev) { 
+    function updateEv(id, ev) { 
         let copy = {...evs};
         // extract the stat from ev
-        copy[ev.slice(0, 2)] = document.getElementById(ev).value; 
+        copy[ev] = document.getElementById(id).value; 
         setevs(copy);
     }
 
     return( 
         <div>
-            <input type="text" onChange={() => updateEv("HPtext")} 
-                value={evs["HP"]} size="3" maxLength="3" id="HPtext" />
-
-            <input onChange={() => updateEv("HP")} type="range" min="0" max="252" 
-                defaultValue="0" value={evs["HP"]} id="HP" />
+            {
+                ["HP", "Atk", "Def", "SpA", "SpD", "Spe"].map(stat => {
+                    return (<EvInput update={updateEv} id={stat} evs={evs}/>);
+            })}
         </div>
     )
 }
---
+++ src/Components/EvInput.js
@@ -0,0 +1,13 @@
+function EvInput(props) { 
+    return(
+        <div>
+            <input type="text" onChange={() => props.update(`${props.id}text`, props.id)} 
+                value={props.evs[props.id]} size="3" maxLength="3" id={`${props.id}text`} />
+
+            <input onChange={() => props.update(props.id, props.id)} type="range" min="0" max="252" 
+                defaultValue="0" value={props.evs[props.id]} id={props.id} />
+        </div>
+    )
+}
+
+export default EvInput;
