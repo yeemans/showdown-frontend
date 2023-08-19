87c30de10fcd1ddb76b5495e5c1819c4ce82c4a2
yeemans <arthur.roth.07@gmail.com>
2023-08-19 16:58:41 -0700

    corrected paths for router

--- showdown-frontend-main/src/RouteSwitch.js
+++ showdown-frontend-main/src/RouteSwitch.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./Components/Home";
 import Navbar from "./Components/Header";
 import Builder from "./Components/Builder";
 
 const RouteSwitch = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/showdown-frontend" exact element={<Home />} />
         <Route path="/showdown-frontend/builder" exact element={<Builder />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
