fd73997ba76d467f6d186412e12f029b21b77298
yeemans <arthur.roth.07@gmail.com>
2023-08-19 17:04:22 -0700

    resolved hashrouter missing error

--- showdown-frontend-main/src/RouteSwitch.js
+++ showdown-frontend-main/src/RouteSwitch.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter} from "react-router-dom";
 import Home from "./Components/Home";
 import Navbar from "./Components/Header";
 import Builder from "./Components/Builder";
